90fa8989a99195fb155b6a3b385c9dff387bb1a2
Akshat-Austin-au8 <akshataustin4321@gmail.com>
2020-08-07 09:14:51 +0530

    routs added

--- sports/src/App.js
+++ sports/src/App.js
@@ -1,24 +1,27 @@
-import React from "react";
+import React, { Component } from "react";
+import { Switch, Route } from "react-router-dom";
+import Country from "./pages/Country/Country";
+import Home from "./pages/Home/Home";
+import AllLeague from "./pages/League/AllLeague";
+import SingleLeague from "./pages/SingleLeague/SingleLeague";
+import SingleTeam from "./pages/SingleTeam/SingleTeam";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render()
+  {
+    return (
+      <React.Fragment>
+        <Switch>
+          <Route exect path="/country" component={Country} />
+          <Route path="/home" component={Home} />
+          <Route path="/allLeague" component={AllLeague} />
+          <Route path="/singleLeague" component={SingleLeague} />
+          <Route path="/singleTeam" component={SingleTeam} />
+        </Switch>
+      </React.Fragment>
+    )
+  };
 }
 
 export default App;
--- sports/src/pages/Home/Home.jsx
+++ sports/src/pages/Home/Home.jsx
@@ -2,7 +2,9 @@ import React, { Component } from "react";
 
 class Home extends Component {
   render() {
-    return <div>This is home page</div>;
+    return (
+      <div>This is home page</div>
+    );
   }
 }
 
