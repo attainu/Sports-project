4f52930db738d7ae1b247a91c8f984725fbb37e7
Lakshay saini <lakshaysaini2013@gmail.com>
2020-08-13 11:24:34 +0530

    v 1.0

--- sports/src/App.js
+++ sports/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import Country from "./pages/Country/Country";
 import Home from "./pages/Home/Home";
 import AllLeague from "./pages/League/AllLeague";
@@ -8,20 +8,20 @@ import SingleTeam from "./pages/SingleTeam/SingleTeam";
 import "./App.css";
 
 class App extends Component {
-  render()
-  {
+  render() {
     return (
-      <React.Fragment>
+      <>
         <Switch>
-          <Route exect path="/country" component={Country} />
-          <Route path="/home" component={Home} />
-          <Route path="/allLeague" component={AllLeague} />
-          <Route path="/singleLeague" component={SingleLeague} />
-          <Route path="/singleTeam" component={SingleTeam} />
+          <Route exact path="/" component={Home} />
+          <Route exact path="/country" component={Country} />
+          <Route exact path="/allLeague" component={AllLeague} />
+          <Route exact path="/singleLeague" component={SingleLeague} />
+          <Route exact path="/singleTeam" component={SingleTeam} />
+          <Redirect to="/" />
         </Switch>
-      </React.Fragment>
-    )
-  };
+      </>
+    );
+  }
 }
 
 export default App;
