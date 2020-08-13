7fded8887b5c2291e30aa4a9accc5d2e117ff384
Lakshay saini <lakshaysaini2013@gmail.com>
2020-08-13 11:08:47 +0530

    Channge in home

--- sports/src/pages/Home/Home.jsx
+++ sports/src/pages/Home/Home.jsx
@@ -3,7 +3,7 @@ import React, { Component } from "react";
 class Home extends Component {
   render() {
     return (
-      <div>This is home page</div>
+      <div>This is home page. This fcommit is to check </div>
     );
   }
 }
