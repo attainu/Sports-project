9d2406615050156b6c29910aa4486f2fb6dd3205
Lakshay saini <lakshaysaini2013@gmail.com>
2020-08-21 22:46:21 +0530

    final ver

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -5,46 +5,47 @@ import Logo from "../../components/Logo/Logo";
 class ContactUs extends Component {
   render() {
     return (
-      <div className="contact">
+      <>
         <Logo />
-        <h1 className="Contact-us">Contact Us</h1>
-        <div className="info">
-          <div className="profile-card">
-            <img
-              src={require("../../assets/img/lakshayimg.jpg")}
-              alt="Lakshay"
-              className="lakshay-img"
-            />
-            <h3 className="profile-name">Lakshay Saini</h3>
-            <p className="information">Frontend Developer</p>
-            <a href="https://www.linkedin.com/in/lakshay-saini-dev/">
+
+        <div className="contact">
+          <h1 className="Contact-us">Contact Us</h1>
+          <div className="info">
+            <div className="profile-card">
               <img
-                src={require("../../assets/img/icons8-linkedin-480.png")}
-                alt="Linkedin-profile"
-                className="linkedin"
+                src={require("../../assets/img/lakshayimg.jpg")}
+                alt="Lakshay"
+                className="lakshay-img"
               />
-            </a>
-          </div>
-          <div className="profile-card">
-            <img
-              src={require("../../assets/img/IMG20200207175040.jpg")}
-              alt="Akshat"
-              className="akshat-img"
-            />
-            <h3 className="profile-name">Lakshay Saini</h3>
-            <p className="information">
-              MERN Stack development trainee at AttainU
-            </p>
-            <a href="https://www.linkedin.com/in/akshat-austin/">
+              <h3 className="profile-name">Lakshay Saini</h3>
+              <p className="information">Frontend Developer</p>
+              <a href="https://www.linkedin.com/in/lakshay-saini-dev/">
+                <img
+                  src={require("../../assets/img/icons8-linkedin-480.png")}
+                  alt="Linkedin-profile"
+                  className="linkedin"
+                />
+              </a>
+            </div>
+            <div className="profile-card">
               <img
-                src={require("../../assets/img/icons8-linkedin-480.png")}
-                alt="Linkedin-profile"
-                className="linkedin"
+                src={require("../../assets/img/IMG20200207175040.jpg")}
+                alt="Akshat"
+                className="akshat-img"
               />
-            </a>
+              <h3 className="profile-name">Lakshay Saini</h3>
+              <p className="information">Frontend Developer</p>
+              <a href="https://www.linkedin.com/in/akshat-austin/">
+                <img
+                  src={require("../../assets/img/icons8-linkedin-480.png")}
+                  alt="Linkedin-profile"
+                  className="linkedin"
+                />
+              </a>
+            </div>
           </div>
         </div>
-      </div>
+      </>
     );
   }
 }
